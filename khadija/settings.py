4610900a4c9097917656a1a384d6c47c82848976
programmer262 <65510490+programmer262@users.noreply.github.com>
2020-10-29 16:35:07 +0100

    Update settings.py

--- khadija/settings.py
+++ khadija/settings.py
@@ -26,64 +26,10 @@ SECRET_KEY = 'gso5fc6^ps3wkk2re^ann3r2v!psc*_-uc%u-f2rd^-rv)#24_'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-JAZZMIN_SETTINGS = {
-  "site_title": "2BAC-PC-F-A-2",
-  "site_header": "2BAC-PC-F-A-2",
-  "site_logo": "docs/jjj.jpg",
-  "welcome_sign": "Welcome to the 2BAC-PC-F-A-2 !!!Only for students of khadija oum elmouminine",
-  "copyright" : "Developers United Ltd",
-  "search_model": "auth.User",
-  "user_avatar": None,
-  "topmenu_links": [
 
-        # Url that gets reversed (Permissions can be added)
-        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
-
-        # external url that opens in a new window (Permissions can be added)
-        {"name": "Support", "url":"https://developers-united.netlify.app", "new_window":True},
-
-        # model admin to link to (Permissions checked against model)
-        {"model": "auth.User"},
-
-        # App with dropdown menu to all its models pages (Permissions checked against models)
-        {"app": "download"},
-    ],
-   "usermenu_links": [
-        {"name": "Support", "url": "https://developers-united.netlify.app", "new_window":True},
-        {"model": "auth.user"}
-    ],
-    "show_sidebar": True,
-    "navigation_expanded": True,
-    "hide_apps": [],
-    "hide_models": [],
-    "order_with_respect_to": ["auth", "download", "download.Etudiant", "download.professor"],
-    "icons": {
-        "auth": "fas fa-users-cog",
-        "auth.user": "fas fa-user",
-        "auth.Group": "fas fa-users",
-        "download.professeur":"fas fa-chalkboard-teacher",
-        "download.etudiant":"fas fa-user-graduate",
-        "download.emploi":"fas fa-hourglass-half",
-        "download.classe":"far fa-eye",
-        "download.live_ended":"far fa-eye-slash",
-        "download.cour":"fas fa-book-reader",
-        "download.exercice":"fas fa-book",
-        "download.corrigé":"fas fa-book",
-        "download.matiére":"fas fa-school",
-        "download.heure":"fas fa-hourglass",
-        "download.présence":"fas fa-graduation-cap"
-
-    },
-    "related_modal_active": True,
-    "changeform_format": "horizontal_tabs",
-    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs",},
-    "language_chooser": False,
-
-}
 
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
